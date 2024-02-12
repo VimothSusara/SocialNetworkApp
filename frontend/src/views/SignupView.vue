a7c9e196ef694816c952c7b5621f743e9fb08eb4
VimothSusara <116062952+VimothSusara@users.noreply.github.com>
2024-02-12 18:48:24 +0530

    created feedView and added imgs

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -1,31 +1,31 @@
 <template>
-    <div class="max-w-2xl mx-auto p-11 bg-white border border-gray-200 rounded-lg shadow-lg">
-        <h1 class="text-center text-4xl font-semibold mb-5 text-purple-900">Signup</h1>
+    <div class="max-w-lg mx-auto py-5 px-9 bg-white border border-gray-200 rounded-lg shadow-lg">
+        <h1 class="text-center text-4xl font-semibold mb-3 text-purple-900">Signup</h1>
 
-        <form class="space-y-6">
+        <form class="space-y-2">
             <div>
-                <label class="text-xl font-normal">Name</label>
+                <label class="text-lg font-normal">Name</label>
                 <input type="text" placeholder="Enter your name"
-                    class="w-full mt-2 py-3 border border-gray-200 rounded-lg pl-2 text-lg">
+                    class="w-full mt-1 py-2 border border-gray-200 rounded-lg pl-2 text-md">
             </div>
             <div>
-                <label class="text-xl font-normal">Email</label>
+                <label class="text-lg font-normal">Email</label>
                 <input type="email" placeholder="Enter your email"
-                    class="w-full mt-2 py-3 border border-gray-200 rounded-lg pl-2 text-lg">
+                    class="w-full mt-1 py-2 border border-gray-200 rounded-lg pl-2 text-md">
             </div>
             <div>
-                <label class="text-xl font-normal">Password</label>
+                <label class="text-lg font-normal">Password</label>
                 <input type="password" placeholder="Enter your password"
-                    class="w-full mt-2 py-3 border border-gray-200 rounded-lg pl-2 mb-1 text-lg">
+                    class="w-full mt-1 py-2 border border-gray-200 rounded-lg pl-2 text-md">
             </div>
             <div>
-                <label class="text-xl font-normal">Repeat Password</label>
+                <label class="text-lg font-normal">Repeat Password</label>
                 <input type="password" placeholder="Repeat your password"
-                    class="w-full mt-2 py-3 border border-gray-200 rounded-lg pl-2 mb-1 text-lg">
+                    class="w-full mt-1 py-2 border border-gray-200 rounded-lg pl-2 text-md">
             </div>
 
             <div class="flex justify-center">
-                <button class="py-3 px-6 bg-purple-600 text-white rounded-xl text-xl">Signup</button>
+                <button class="py-2 px-5 bg-purple-600 text-white rounded-xl text-xl mt-2">Signup</button>
             </div>
 
             <p class="font-medium text-center text-xl">
