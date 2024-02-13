c89e84eff407b560ad62fad950ba15a584f0589d
VimothSusara <116062952+VimothSusara@users.noreply.github.com>
2024-02-13 21:42:54 +0530

    create messages page and add path

---
+++ frontend/src/views/MessagesView.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="max-w-8xl mx-auto grid grid-cols-4 gap-4">
+        <div class="main-left col-span-1">
+            <div class="p-4 bg-white border border-gray-200 rounded-lg">
+                <div class="space-y-4">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center space-x-2">
+                            <img src="https://i.pravatar.cc/300?img=56" class="w-[40px] rounded-full">
+                            <p class="text-s"><strong>Code with me</strong></p>
+                        </div>
+                        <span class="text-s text-gray-500">20 minutes ago</span>
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center space-x-2">
+                            <img src="https://i.pravatar.cc/300?img=56" class="w-[40px] rounded-full">
+                            <p class="text-s"><strong>Code with me</strong></p>
+                        </div>
+                        <span class="text-s text-gray-500">20 minutes ago</span>
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center space-x-2">
+                            <img src="https://i.pravatar.cc/300?img=56" class="w-[40px] rounded-full">
+                            <p class="text-s"><strong>Code with me</strong></p>
+                        </div>
+                        <span class="text-s text-gray-500">20 minutes ago</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-right col-span-3 space-y-4">
+            <div class="p-4 bg-white border border-gray-200 rounded-lg">
+                <div class="flex flex-col flex-grow p-4">
+                    <div class="flex w-full mt-2 space-x-3 max-w-2xl ml-auto justify-end">
+                        <div>
+                            <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                                <p class="text-s">Lorem ipsum dolor sit amet consectetur adipisicing elit.Tenetur at
+                                    consequuntur officia, facilis esse reiciendis reprehenderit dignissimos
+                                    similique. Hic, id!</p>
+                            </div>
+                            <span class="text-s text-gray-500 leading-none">4 minutes ago</span>
+                        </div>
+                        <div class="flex-shrink-0 h-12 w-12 rounded-full bg-gray-300">
+                            <img src="https://i.pravatar.cc/300?img=56" class="w-[50px] rounded-full" alt="">
+                        </div>
+                    </div>
+                    <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
+                        <div>
+                            <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                                <p class="text-s">Lorem ipsum dolor sit amet consectetur adipisicing elit.Tenetur</p>
+                            </div>
+                            <span class="text-s text-gray-500 leading-none">9 minutes ago</span>
+                        </div>
+                        <div class="flex-shrink-0 h-12 w-12 rounded-full bg-gray-300">
+                            <img src="https://i.pravatar.cc/300?img=56" class="w-[50px] rounded-full" alt="">
+                        </div>
+                    </div>
+                    <div class="flex w-full mt-2 space-x-3 max-w-md mr-auto justify-start">
+                        <div class="flex-shrink-0 h-12 w-12 rounded-full bg-gray-300">
+                            <img src="https://i.pravatar.cc/300?img=56" class="w-[50px] rounded-full" alt="">
+                        </div>
+                        <div>
+                            <div class="bg-gray-300 p-3 rounded-r-lg rounded-br-lg">
+                                <p class="text-s">Lorem ipsum dolor sit amet consectetur adipisicing elit.Tenetur</p>
+                            </div>
+                            <span class="text-s text-gray-500 leading-none">9 minutes ago</span>
+                        </div>
+                    </div>
+                    <div class="flex w-full mt-2 space-x-3 max-w-2xl mr-auto justify-start">
+                        <div class="flex-shrink-0 h-12 w-12 rounded-full bg-gray-300">
+                            <img src="https://i.pravatar.cc/300?img=56" class="w-[50px] rounded-full" alt="">
+                        </div>
+                        <div>
+                            <div class="bg-gray-300 p-3 rounded-r-lg rounded-br-lg">
+                                <p class="text-s">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis vitae
+                                    alias voluptatum molestiae praesentium asperiores cupiditate illum. Ratione?</p>
+                            </div>
+                            <span class="text-s text-gray-500 leading-none">9 minutes ago</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="p-4 bg-white border border-gray-200 rounded-lg">
+                <div class="p-4 bg-white border border-gray-200 rounded-lg">
+                    <div class="p-4">
+                        <textarea class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What are you thinking?"></textarea>
+                    </div>
+                    <div class="p-4 border-t border-gray-100 flex justify-start">
+                        <a href="#"
+                            class="inline-block py-3 px-5 bg-purple-600 text-white rounded-lg text-xl font-bold">Post</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
