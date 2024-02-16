4677fef53d182db583c7be91a432470944a3ce15
VimothSusara <116062952+VimothSusara@users.noreply.github.com>
2024-02-16 21:46:29 +0530

    created search page, added reusable components

---
+++ frontend/src/components/PeopleYouMayKnow.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="p-4 bg-white border border-gray-200 rounded-lg">
+        <h3 class="mb-6 text-2xl font-semibold">People you may know</h3>
+
+        <div class="space-y-4">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center space-x-2">
+                    <img src="https://i.pravatar.cc/300?img=56" class="w-[40px] rounded-full">
+                    <p class="text-s"><strong>Code with me</strong></p>
+                </div>
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-s rounded-lg">Show</a>
+            </div>
+            <div class="flex items-center justify-between">
+                <div class="flex items-center space-x-2">
+                    <img src="https://i.pravatar.cc/300?img=56" class="w-[40px] rounded-full">
+                    <p class="text-s"><strong>Code with me</strong></p>
+                </div>
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-s rounded-lg">Show</a>
+            </div>
+            <div class="flex items-center justify-between">
+                <div class="flex items-center space-x-2">
+                    <img src="https://i.pravatar.cc/300?img=56" class="w-[40px] rounded-full">
+                    <p class="text-s"><strong>Code with me</strong></p>
+                </div>
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-s rounded-lg">Show</a>
+            </div>
+        </div>
+
+    </div>
+</template>
--- frontend/src/views/FeedView.vue
+++ frontend/src/views/FeedView.vue
@@ -108,76 +108,14 @@
             </div>
         </div>
         <div class="main-right col-span-1 space-y-4">
-            <div class="p-4 bg-white border border-gray-200 rounded-lg">
-                <h3 class="mb-6 text-2xl font-semibold">People you may know</h3>
-
-                <div class="space-y-4">
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <img src="https://i.pravatar.cc/300?img=56" class="w-[40px] rounded-full">
-                            <p class="text-s"><strong>Code with me</strong></p>
-                        </div>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-s rounded-lg">Show</a>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <img src="https://i.pravatar.cc/300?img=56" class="w-[40px] rounded-full">
-                            <p class="text-s"><strong>Code with me</strong></p>
-                        </div>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-s rounded-lg">Show</a>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <img src="https://i.pravatar.cc/300?img=56" class="w-[40px] rounded-full">
-                            <p class="text-s"><strong>Code with me</strong></p>
-                        </div>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-s rounded-lg">Show</a>
-                    </div>
-                </div>
-
-            </div>
-            <div class="p-4 bg-white border border-gray-200 rounded-lg">
-                <h3 class="mb-6 text-2xl font-semibold">Trends</h3>
-
-                <div class="space-y-4">
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <p class="text-s">
-                                <strong>#codewithme</strong><br>
-                                <span class="text-gray-500">30 posts</span>
-                            </p>
-                        </div>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-s rounded-lg">Explore</a>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <p class="text-s">
-                                <strong>#codewithme</strong><br>
-                                <span class="text-gray-500">30 posts</span>
-                            </p>
-                        </div>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-s rounded-lg">Explore</a>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <p class="text-s">
-                                <strong>#codewithme</strong><br>
-                                <span class="text-gray-500">30 posts</span>
-                            </p>
-                        </div>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-s rounded-lg">Explore</a>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <p class="text-s">
-                                <strong>#codewithme</strong><br>
-                                <span class="text-gray-500">30 posts</span>
-                            </p>
-                        </div>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-s rounded-lg">Explore</a>
-                    </div>
-                </div>
-            </div>
+            <PeopleYouMayKnow />
+            <Trends />
         </div>
     </div>
 </template>
+
+<script setup>
+import PeopleYouMayKnow from '@/components/PeopleYouMayKnow.vue';
+import Trends from '@/components/Trends.vue';
+
+</script>
